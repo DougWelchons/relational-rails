7010329a7efbf66a8a8e8aba70dd10e557b81a78
DougWelchons <doug.welchons@gmail.com>
2021-02-02 15:56:30 -0700

    Add show member feature

--- spec/features/members/show_spec.rb
+++ spec/features/members/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'as a visitor' do
     it "should show the parent and all of it's attributes" do
       member1 = Member.create!(name:"Doug", premium_member:true, skill_level:3)
 
-      visit "/members/1"
+      visit "/members/#{member1.id}"
 
       expect(page).to have_content(member1.id)
       expect(page).to have_content(member1.name)
@@ -12,6 +12,7 @@ RSpec.describe 'as a visitor' do
       expect(page).to have_content(member1.skill_level)
       expect(page).to have_content(member1.created_at)
       expect(page).to have_content(member1.updated_at)
+      save_and_open_page
     end
   end
 end
