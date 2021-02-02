2969a719280010aedbc106bcd04945eb6728b0b4
DougWelchons <doug.welchons@gmail.com>
2021-02-02 15:16:51 -0700

    Add minor refactors

--- spec/features/members/index_spec.rb
+++ spec/features/members/index_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
-RSpec.describe "members index page" do
-  describe "as a visitor" do
-    describe "when i visit members/index" do
-      it "shows each member in the system" do
-        member1 = Member.create!(name:"Doug", premium_member:true, skill_level:3)
-        member2 = Member.create!(name:"Sam", premium_member:false, skill_level:5)
+RSpec.describe "as a visitor" do
+  describe "when i visit members/index" do
+    it "shows each member in the system" do
+      member1 = Member.create!(name:"Doug", premium_member:true, skill_level:3)
+      member2 = Member.create!(name:"Sam", premium_member:false, skill_level:5)
 
-        visit "/members"
+      visit "/members"
 
-        expect(page).to have_content(member1.name)
-        expect(page).to have_content(member2.name)
-      end
+      expect(page).to have_content(member1.name)
+      expect(page).to have_content(member2.name)
     end
   end
 end
