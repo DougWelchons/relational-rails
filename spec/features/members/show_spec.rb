bb8e878fa4482bd92aca192218ce1422b4fb573a
DougWelchons <doug.welchons@gmail.com>
2021-02-02 15:29:12 -0700

    Add test for story 2

---
+++ spec/features/members/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+RSpec.describe 'as a visitor' do
+  describe "When I visit '/parents/:id'" do
+    it "should show the parent and all of it's attributes" do
+      member1 = Member.create!(name:"Doug", premium_member:true, skill_level:3)
+
+      visit "/members/1"
+
+      expect(page).to have_content(member1.id)
+      expect(page).to have_content(member1.name)
+      expect(page).to have_content(member1.premium_member)
+      expect(page).to have_content(member1.skill_level)
+      expect(page).to have_content(member1.created_at)
+      expect(page).to have_content(member1.updated_at)
+    end
+  end
+end
