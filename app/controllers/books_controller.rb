465314700b8c138a3fe68d9aad5782fb997647d3
Megan Campbell <megan.campbell26@yahoo.com>
2021-02-07 11:15:54 -0700

    User story 9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,30 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update]
 
   def index
     @books = Book.all
   end
 
   def show
+    @book
+  end
+
+  def edit
+    @book
+  end
+  
+  def update
+    @book = Book.update(book_params)
+    
+    redirect_to "/books/#{params[:id]}"
+  end
+
+  private
+  def set_book
     @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.permit(:name, :pages, :available)
   end
 end
