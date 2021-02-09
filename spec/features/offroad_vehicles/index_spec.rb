6fa8f04bb759bcb0998cd525c5945e8c6b241cb5
DougWelchons <doug.welchons@gmail.com>
2021-02-08 17:51:07 -0700

    Add test for user story 7

--- spec/features/offroad_vehicles/index_spec.rb
+++ spec/features/offroad_vehicles/index_spec.rb
@@ -21,4 +21,24 @@ RSpec.describe "ORV index page" do
       expect(page).to have_content("Ford, F-250")
     end
   end
+
+  # User Story 7
+  describe "when I visit /members/:id/offroad_vehicles" do
+    it "shows a list of off road vehicles belonging to that member" do
+      member1 = Member.create!(name: "Doug", premium_member: true, skill_level: 3)
+      member2 = Member.create!(name: "John", premium_member: false, skill_level: 2)
+
+      member1.offroad_vehicles.create!(name: "Black Sheep", make: "Jeep", model: "Wrangler", tire_size: 33, passed_safety_inspection: true)
+      member1.offroad_vehicles.create!(name: "Dragon-Alligator", make: "Jeep", model: "Gladiator", tire_size: 37, passed_safety_inspection: true)
+      member2.offroad_vehicles.create!(name: "BigBoi", make: "Ford", model: "F-250", tire_size: 37, passed_safety_inspection: true)
+      member2.offroad_vehicles.create!(name: "Monster", make: "Jeep", model: "Gladiator", tire_size: 30, passed_safety_inspection: true)
+
+      visit "/members/#{member1.id}/offroad_vehicles"
+
+      expect(page).to have_content("Black Sheep")
+      expect(page).to have_content("Jeep, Wrangler")
+      expect(page).to_not have_content("BigBoi")
+      expect(page).to_not have_content("Ford, F-250")
+    end
+  end
 end
